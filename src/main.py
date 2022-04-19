1b820ec1cbc11b8fbcceae597dd01a6af8e1d15a
Morale <lhzmrl@outlook.com>
2022-04-20 07:20:05 +0800

    Fix import version module error

--- src/main.py
+++ src/main.py
@@ -33,28 +33,25 @@ def parseELF(fname, **kwargs):
 def loadLibraries(blob):
     stream = BytesIO(blob)
     stream.seek(20)
-    version = stream.read(32).decode('UTF-8')
+    snapshotHash = stream.read(32).decode('UTF-8')
     
     SUPPORTED_SNAPSHOT = {
-        "v2_10": "8ee4ef7a67df9845fba331734198a953",
-        "v2_12": "5b97292b25f0a715613b7a28e0734f77"
+        "8ee4ef7a67df9845fba331734198a953": "v2_10",
+        "5b97292b25f0a715613b7a28e0734f77": "v2_12"
     }
 
     WIP_SNAPSHOT = {
-        "v2_13": "e4a09dbf2bb120fe4674e0576617a0dc"
+        "e4a09dbf2bb120fe4674e0576617a0dc": "v2_13"
     }
 
-    for key,value in SUPPORTED_SNAPSHOT.items():
-        if version in value:
-            snapshotModule = importlib.import_module('{}.Snapshot'.format(key))
-            resolverModule = importlib.import_module('{}.Resolver'.format(key))
-            classIdModule = importlib.import_module('{}.ClassId'.format(key))
-            
-    for key,value in WIP_SNAPSHOT.items():
-        raise Exception('Still Work in Progress for Dart SDK ' + key)
-    
-    if version not in SUPPORTED_SNAPSHOT.values() and version not in WIP_SNAPSHOT.values():
-        raise Exception('Unsupported Dart SDK Version: ' + version)
+    if snapshotHash in SUPPORTED_SNAPSHOT.keys():
+        snapshotModule = importlib.import_module('{}.Snapshot'.format(SUPPORTED_SNAPSHOT[snapshotHash]))
+        resolverModule = importlib.import_module('{}.Resolver'.format(SUPPORTED_SNAPSHOT[snapshotHash]))
+        classIdModule = importlib.import_module('{}.ClassId'.format(SUPPORTED_SNAPSHOT[snapshotHash]))      
+    elif snapshotHash in WIP_SNAPSHOT.keys():
+        raise Exception('Still Work in Progress for Dart SDK ' + WIP_SNAPSHOT[snapshotHash])
+    else:
+        raise Exception('Unsupported Dart SDK, snapshot hash: ' + snapshotHash)
     
     global Snapshot
     Snapshot = getattr(snapshotModule, 'Snapshot')
